dbdd14b6aff0205061e85b63271c86bd625951b5
vkflco08 <67574367+vkflco08@users.noreply.github.com>
2024-01-14 20:31:40 +0900

    ADD jwt token

---
+++ src/main/kotlin/com/example/judo/common/authority/JwtTokenProvider.kt
@@ -0,0 +1,95 @@
+package com.example.judo.common.authority
+
+import io.jsonwebtoken.*
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+import java.util.*
+
+const val EXPIRATION_MILLISECONDS: Long = 1000 * 60 * 30
+
+@Component
+class JwtTokenProvider {
+    @Value("\${jwt.secret}")
+    lateinit var secretKey: String
+
+    private val key by lazy { Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey))}
+
+    /**
+     * Token 생성
+     */
+    fun createToken(authentication: Authentication): TokenInfo {
+        val authorities: String = authentication
+            .authorities
+            .joinToString(",", transform = GrantedAuthority::getAuthority)
+
+        val now = Date()
+        val accessExpiration = Date(now.time + EXPIRATION_MILLISECONDS)
+
+        // Access Token
+        val accessToken = Jwts
+            .builder()
+            .setSubject(authentication.name)
+            .claim("auth", authorities)
+            .setIssuedAt(now)
+            .setExpiration(accessExpiration)
+            .signWith(key, SignatureAlgorithm.HS256)
+            .compact()
+
+        return TokenInfo("Bearer", accessToken)
+    }
+
+    /**
+     * Token 정보 추출
+     */
+
+    fun getAuthentication(token: String): Authentication{
+        val claims : Claims = getClaims(token)
+
+        // claims에 있는 auth를 가져온다
+        val auth = claims["auth"] ?: throw RuntimeException("잘못된 토큰입니다.")
+
+        // 권한 정보 추출
+        val authorities: Collection<GrantedAuthority> = (auth as String)
+            .split(",")
+            .map{ SimpleGrantedAuthority(it) }
+
+        val principal: UserDetails = User(claims.subject, "", authorities)
+
+        return UsernamePasswordAuthenticationToken(principal, "", authorities)
+    }
+
+    /**
+     * Token 검증
+     */
+    fun validateToken(token: String): Boolean{
+        try{
+            getClaims(token)
+            return true
+        } catch (e: Exception){
+            when(e){
+                is SecurityException -> {}
+                is MalformedJwtException -> {}
+                is ExpiredJwtException -> {}
+                is UnsupportedJwtException -> {}
+                is IllegalArgumentException -> {}
+                else -> {}
+            }
+            println(e.message)
+        }
+        return false
+    }
+    private fun getClaims(token: String): Claims =
+        Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .body
+}
